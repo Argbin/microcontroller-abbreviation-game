e9e2c0580e04d199673e010e0aa6ee76dd0d16b4
Albin Persson <Albin.f.persson@outlook.com>
2026-09-14 19:58:21 +0200

    Create "Player" class
    The class contains:
    String playerName
    int score
    getPlayerName()
    setPlayerName()
    getScore()
    resetScore()
    addPoint()
    Player()

--- src/quizgame/model/Player.java
+++ src/quizgame/model/Player.java
@@ -1,4 +1,29 @@
 package quizgame.model;
 
 public class Player {
+    private String playerName;
+    private int score;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    public void resetScore(int score) {
+        this.score = 0;
+    }
+
+    public void addPoint() {
+        score++;
+    }
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+        this.score = 0;
+    }
 }
