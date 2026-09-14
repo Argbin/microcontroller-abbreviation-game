0f18e56946225d497840fc4b534b9a6229a0bd24
Albin Persson <Albin.f.persson@outlook.com>
2026-09-14 20:13:51 +0200

    Create "QuestionBank" class
    The class contains:
    List<Question> questions
    QuestionBank()
    shuffleQuestions()
    hasQuestions()
    getNextQuestion()

--- src/quizgame/model/QuestionBank.java
+++ src/quizgame/model/QuestionBank.java
@@ -1,4 +1,28 @@
 package quizgame.model;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
 public class QuestionBank {
+    private List<Question> questions;
+
+    public QuestionBank(List<Question> initialQuestions) {
+        this.questions = new ArrayList<>(initialQuestions);
+    }
+
+    public void shuffleQuestions() {
+        Collections.shuffle(questions);
+    }
+
+    public boolean hasQuestions() {
+        return !questions.isEmpty();
+    }
+
+    public Question getNextQuestion() {
+        if (hasQuestions()) {
+            return questions.removeFirst();
+        }
+        return null;
+    }
 }
